f2209b741196ccd90fddcff8215903ce2ef4f3be
Fillipe <fillipe484@gmail.com>
2021-06-02 20:51:24 -0300

    slider

--- src/components/AnimeCards/CardsList.tsx
+++ src/components/AnimeCards/CardsList.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
 
-import '../../styles/global.scss';
-import styles from './styles.module.scss';
+import { Swiper, SwiperSlide } from 'swiper/react';
+// Import Swiper styles
+import 'swiper/components/pagination/pagination.min.css';
+import 'swiper/swiper.min.css';
+
+// import Swiper core and required modules
+import SwiperCore, { Navigation, Pagination } from 'swiper/core';
+
 import animes from '../../database/mock.json';
+import styles from './styles.module.scss';
+import '../../styles/global.scss';
+
+// install Swiper modules
+SwiperCore.use([Navigation, Pagination]);
 
 const CardsList: React.FC = () => {
   return (
@@ -10,62 +21,81 @@ const CardsList: React.FC = () => {
       <div className={styles.cardlistContainer}>
         <h1>Novas Dublagens</h1>
         <br />
-        <div className={styles.showWrapper}>
-          <section className={styles.showContainer}>
-            {animes.map(anime => (
-              <img src={anime.image} key={anime.id} alt="img" />
-            ))}
-          </section>
-        </div>
-        <br />
+
+        <section>
+          <Swiper slidesPerView={6} setWrapperSize>
+            <SwiperSlide className={styles.SwiperCards}>
+              {animes.map(anime => (
+                <img src={anime.image} key={anime.id} alt="img" />
+              ))}
+            </SwiperSlide>
+          </Swiper>
+        </section>
+
         <h1>Em Alta</h1>
         <br />
-        <div className={styles.showWrapper}>
-          <section className={styles.showContainer}>
-            {animes.map(anime => (
-              <img src={anime.image} key={anime.id} alt="img" />
-            ))}
-          </section>
-        </div>
+
+        <section>
+          <Swiper slidesPerView={6} className="mySwiper">
+            <SwiperSlide className={styles.SwiperCards}>
+              {animes.map(anime => (
+                <img src={anime.image} key={anime.id} alt="img" />
+              ))}
+            </SwiperSlide>
+          </Swiper>
+        </section>
+
         <h1>Mais Populares</h1>
         <br />
-        <div className={styles.showWrapper}>
-          <section className={styles.showContainer}>
-            {animes.map(anime => (
-              <img src={anime.image} key={anime.id} alt="img" />
-            ))}
-          </section>
-        </div>
-        <br />
+
+        <section>
+          <Swiper slidesPerView={6} className="mySwiper">
+            <SwiperSlide className={styles.SwiperCards}>
+              {animes.map(anime => (
+                <img src={anime.image} key={anime.id} alt="img" />
+              ))}
+            </SwiperSlide>
+          </Swiper>
+        </section>
+
         <h1>Streaming Somente na Funimation</h1>
         <br />
-        <div className={styles.showWrapper}>
-          <section className={styles.showContainer}>
-            {animes.map(anime => (
-              <img src={anime.image} key={anime.id} alt="img" />
-            ))}
-          </section>
-        </div>
-        <br />
+
+        <section>
+          <Swiper slidesPerView={6} className="mySwiper">
+            <SwiperSlide className={styles.SwiperCards}>
+              {animes.map(anime => (
+                <img src={anime.image} key={anime.id} alt="img" />
+              ))}
+            </SwiperSlide>
+          </Swiper>
+        </section>
+
         <h1>O Que Assistir</h1>
         <br />
-        <div className={styles.showWrapper}>
-          <section className={styles.showContainer}>
-            {animes.map(anime => (
-              <img src={anime.image} key={anime.id} alt="img" />
-            ))}
-          </section>
-        </div>
-        <br />
+
+        <section>
+          <Swiper slidesPerView={6} className="mySwiper">
+            <SwiperSlide className={styles.SwiperCards}>
+              {animes.map(anime => (
+                <img src={anime.image} key={anime.id} alt="img" />
+              ))}
+            </SwiperSlide>
+          </Swiper>
+        </section>
+
         <h1>Explorar</h1>
         <br />
-        <div className={styles.showWrapper}>
-          <section className={styles.showContainer}>
-            {animes.map(anime => (
-              <img src={anime.image} key={anime.id} alt="img" />
-            ))}
-          </section>
-        </div>
+
+        <section>
+          <Swiper slidesPerView={6} className="mySwiper">
+            <SwiperSlide className={styles.SwiperCards}>
+              {animes.map(anime => (
+                <img src={anime.image} key={anime.id} alt="img" />
+              ))}
+            </SwiperSlide>
+          </Swiper>
+        </section>
       </div>
     </>
   );
